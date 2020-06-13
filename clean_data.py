7582a8fca8490b5b99acfd0d869ac02052309df0
TJ Combs <tjcombs92@gmail.com>
2020-06-13 13:52:35 -1000

    Commented the code

--- clean_data.py
+++ clean_data.py
@@ -34,8 +34,10 @@ def read_all_zcta_data(data_dir = '../coronavirus-data/', historical_data_dir = 
         when the data was created.
 
     '''
+    # Access the coronavirus-data repo
     repo = Repo(data_dir)
     
+    # Get information about the commits
     commit_history = pd.DataFrame({'GIT_COMMIT': repo.iter_commits()})
     commit_history['COMMIT'] = commit_history['GIT_COMMIT'].astype(str)
     commit_history['COMMIT_FIRST7'] = commit_history['COMMIT'].str[:7]
@@ -45,49 +47,58 @@ def read_all_zcta_data(data_dir = '../coronavirus-data/', historical_data_dir = 
     # Only keeping commits that possibly have a date in it
     commit_history = commit_history[commit_history['MESSAGE'].str.contains('[0-9].+[0-9]')]
     
-    
+    # Try to extract a date from the commit message
+    # TODO: Compare to the commit message to make sure what we got was reasonable
     commit_history['DATA_DATE_STR'] = commit_history['MESSAGE'].str.extract('([0-9][0-9\./]+[0-9])')[0]
     commit_history['DATA_DATE_STR'] = commit_history['DATA_DATE_STR'].str.replace('\.', '/')
     
-    # TODO: Compare to the commit message to make sure what we got was reasonable
-    # Make sure message quality issues are taken care of
+    # Convert the date into a datetime object
     commit_history['DATA_DATE'] = commit_history['DATA_DATE_STR'].apply(datestring2020_to_datetime)
     
-    # Choose the most recent commit for every data date
+    # For each data date, choose the row with the most recent commit
     commit_history = commit_history.sort_values('COMMITTED_DATETIME', ascending=False)
     commit_history = commit_history.drop_duplicates('DATA_DATE')
     
-    # Get the file names
+    # Get the file names along with information on how to tie them back to the 
+    # commits
     historical_data = pd.DataFrame({'FILENAME': os.listdir('historical_data')})
     historical_data['FILEPATH'] = historical_data_dir + historical_data['FILENAME']
     historical_data['COMMIT_FIRST7'] = historical_data['FILENAME'].str.split('.').str[-2]
     historical_data['FILETYPE'] = historical_data['FILENAME'].str.split('.').str[0]
-    # If data-by-modzcta and tests-by-zcta show up on the same commit, choose data-by-modzcta
+    
+    # If data-by-modzcta and tests-by-zcta show up on the same commit, choose 
+    # data-by-modzcta
     historical_data = historical_data.sort_values('FILETYPE')
     historical_data = historical_data.drop_duplicates('COMMIT_FIRST7')
-    
     
     # Attach the filepath to the commit history
     commit_history = pd.merge(left=historical_data, right=commit_history, on='COMMIT_FIRST7')
     
+    # Read all the csvs as pandas DataFrames
     commit_history['DATA'] = commit_history.apply(lambda x: read_zcta_data(x['FILEPATH'], x['DATA_DATE']), axis=1)
-    commit_history['COLUMNS'] = commit_history['DATA'].apply(lambda x: tuple(sorted(x)))
     
     # Final Data
     data = pd.concat(list(commit_history['DATA']))
     
+    # Many of the older records have a null NEIGHBORHOOD_NAME and BOROUGH_GROUP.
+    # Fill in the old data with the newer data.
     zcta_info = data[['MODIFIED_ZCTA', 'NEIGHBORHOOD_NAME', 'BOROUGH_GROUP', 'DATA_DATE']].dropna()
-    # Taking the most recent nieghborbhood name and borough group
+    # Take the most recent nieghborbhood name and borough group
     zcta_info = zcta_info.sort_values('DATA_DATE', ascending=False)
     zcta_info = zcta_info[['MODIFIED_ZCTA', 'NEIGHBORHOOD_NAME', 'BOROUGH_GROUP']].drop_duplicates('MODIFIED_ZCTA')
+    # Remove this fields from the original data
     data = data[[x for x in data if x not in ['NEIGHBORHOOD_NAME', 'BOROUGH_GROUP']]]
+    # Attach the new values to the data
     data = pd.merge(left=data, right=zcta_info, how='left')
     
-    # The POP_DENOMINATOR is the same for all the csvs
+    # The POP_DENOMINATOR field is null for the older data
+    # The POP_DENOMINATOR is the same for all csvs which contain it.  Attach
+    # This to the older data.
     pop_denominator = data[['MODIFIED_ZCTA', 'POP_DENOMINATOR']].drop_duplicates()
     pop_denominator = pop_denominator[pop_denominator['POP_DENOMINATOR'].notnull()]
     assert((~pop_denominator['MODIFIED_ZCTA'].duplicated()).all())
     data = data[[x for x in data if x!='POP_DENOMINATOR']]
+    # Attach POP_DENOMINATOR to all the data
     data = pd.merge(left=data, right=pop_denominator, on='MODIFIED_ZCTA', how='left')
 
     return data
